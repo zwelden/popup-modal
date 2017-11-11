b1807855e513bdf0e9c5f79dd52dd818c015e592
Zach Welden <zwelden@broco.com>
2017-11-11 17:14:46 -0600

    add createModule function

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,5 @@
+/* one-var-declaration-per-line: ["error", "always"]*/
+
 (function (app) {
   /*
   *
@@ -9,12 +11,12 @@
     var isModalCreated = false;
 
     // base modal content
-    var popupModal,
-      popupModalInner,
-      popupModalClass = 'popup-modal',
-      popupContentHolder,
-      popupContentHolderInner,
-      popupModalContentClass = 'content-holder';
+    var popupModal;
+    var popupModalInner;
+    var popupModalClass = 'popup-modal';
+    var popupContentHolder;
+    var popupContentHolderInner;
+    var popupContentClass = 'content-holder';
 
     var createModal = function () {
       if (isModalCreated === false) {
@@ -22,9 +24,18 @@
         popupModal = document.createElement('div');
         popupModal.classList.add(popupModalClass + '__wrapper');
         popupModalInner = document.createElement('div');
-        popupModalInner.classList.add(popupModalClass);
+        popupModalInner.classList.add(popupModalClass + '__inner');
+        popupModal.append(popupModalInner);
 
         // create popup modal content holder
+        popupContentHolder = document.createElement('div');
+        popupContentHolder.classList.add(popupContentClass + '__wrapper');
+        popupContentHolderInner = document.createElement('div');
+        popupContentHolderInner.classList.add(popupContentClass + '__inner');
+        popupContentHolder.append(popupContentHolderInner);
+
+        // apend content holder to modal
+        popupModalInner.append(popupContentHolder);
 
         // append modal to body
         document.querySelector('body').append(popupModal);
