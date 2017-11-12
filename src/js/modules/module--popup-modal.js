b1abd4598a1d2a5150ed207ebbc31539a0d30a8e
Zach Welden <zwelden@broco.com>
2017-11-11 21:45:22 -0600

    add fill modal function

--- src/js/modules/module--popup-modal.js
+++ src/js/modules/module--popup-modal.js
@@ -1,6 +1,4 @@
-/* one-var-declaration-per-line: ["error", "always"]*/
-
-(function (app) {
+(function (app, window, document) {
   /*
   *
   * Documentation
@@ -11,42 +9,50 @@
     var isModalCreated = false;
 
     // base modal content
+    var popupModalWrapper;
     var popupModal;
-    var popupModalInner;
     var popupModalClass = 'popup-modal';
+    var popupContentHolderWrapper;
     var popupContentHolder;
-    var popupContentHolderInner;
     var popupContentClass = 'content-holder';
 
     var createModal = function () {
       if (isModalCreated === false) {
         // create modal element and assign it wrapper class
+        popupModalWrapper = document.createElement('div');
+        popupModalWrapper.classList.add(popupModalClass + '__wrapper');
         popupModal = document.createElement('div');
-        popupModal.classList.add(popupModalClass + '__wrapper');
-        popupModalInner = document.createElement('div');
-        popupModalInner.classList.add(popupModalClass + '__inner');
-        popupModal.append(popupModalInner);
+        popupModal.classList.add(popupModalClass);
+        popupModalWrapper.append(popupModal);
 
         // create popup modal content holder
+        popupContentHolderWrapper = document.createElement('div');
+        popupContentHolderWrapper.classList.add(popupContentClass + '__wrapper');
         popupContentHolder = document.createElement('div');
-        popupContentHolder.classList.add(popupContentClass + '__wrapper');
-        popupContentHolderInner = document.createElement('div');
-        popupContentHolderInner.classList.add(popupContentClass + '__inner');
-        popupContentHolder.append(popupContentHolderInner);
+        popupContentHolder.classList.add(popupContentClass);
+        popupContentHolderWrapper.append(popupContentHolder);
 
         // apend content holder to modal
-        popupModalInner.append(popupContentHolder);
+        popupModal.append(popupContentHolderWrapper);
 
         // append modal to body
-        document.querySelector('body').append(popupModal);
+        document.querySelector('body').append(popupModalWrapper);
 
         // update modal created status
         isModalCreated = true;
       }
     };
 
-    var fillModal = function () {
-      // ...
+    /**
+    * @param targetClass string -- class identifier for targeting
+    * the element in the document tree to load into the popup modal
+    * Targeted element will be deep cloned (minus event listeners).
+    */
+    var fillModal = function (targetClass) {
+      var targetObj = document.querySelector('.' + targetClass);
+      var copyTarget = targetObj.cloneNode(true);
+      popupContentHolder.innerHTML = '';
+      popupContentHolder.appendChild(copyTarget);
     };
 
     var activateModal = function () {
@@ -68,4 +74,4 @@
       closeModal: closeModal
     };
   })();
-})(window.app = window.app || {});
+})(window.app = window.app || {}, window, document);
